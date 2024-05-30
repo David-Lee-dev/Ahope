36cf8172f90808ee59ad35b88c2ee0634b55bfeb
David-Lee-dev <aganga7427@gmail.com>
2024-05-30 17:20:08 +0900

    feat: add uncollected ui

--- client/lib/widget/collectionCard.widget.dart
+++ client/lib/widget/collectionCard.widget.dart
@@ -20,7 +20,7 @@ class _CollcetionCardState extends State<CollcetionCard> {
 
   @override
   void initState() {
-    Future.delayed(const Duration(milliseconds: 500)).then((_) {
+    Future.delayed(const Duration(milliseconds: 1000)).then((_) {
       if (mounted) {
         setState(() {
           showImage = true;
@@ -30,79 +30,123 @@ class _CollcetionCardState extends State<CollcetionCard> {
     super.initState();
   }
 
+  bool _isCollected() {
+    return widget.data.cards != null && widget.data.cards!.isNotEmpty;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
         ClipPath(
           clipper: TopRightClipper(clipSize: 37),
-          child: Container(
-            clipBehavior: Clip.hardEdge,
-            decoration: BoxDecoration(
-              gradient: const LinearGradient(
-                begin: Alignment(-1.0, -1.0),
-                end: Alignment(1.0, 2.0),
-                colors: [Color(0xff5c5ae4), Color(0xffDE4981)],
+          child: _isCollected()
+              ? _buildCardContent()
+              : ColorFiltered(
+                  colorFilter: const ColorFilter.matrix(<double>[
+                    0.2126,
+                    0.7152,
+                    0.0722,
+                    0,
+                    0,
+                    0.2126,
+                    0.7152,
+                    0.0722,
+                    0,
+                    0,
+                    0.2126,
+                    0.7152,
+                    0.0722,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    1,
+                    0,
+                  ]),
+                  child: _buildCardContent(),
+                ),
+        ),
+        if (_isCollected())
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                ' No. ${widget.data.cards![0].seq}',
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              borderRadius: BorderRadius.circular(8),
-            ),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const SizedBox(height: 28),
-                AspectRatio(
-                  aspectRatio: 1,
-                  child: Container(
-                    clipBehavior: Clip.hardEdge,
-                    decoration: BoxDecoration(
-                      color: const Color(0xff364458),
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Image.network(
-                      widget.data.imageUrl,
-                      loadingBuilder: (context, child, loadingProgress) {
-                        bool imageLoaded = false;
-                        if (loadingProgress == null) imageLoaded = true;
-
-                        return showImage && imageLoaded
-                            ? child
-                            : Shimmer.fromColors(
-                                baseColor: const Color(0xff051732),
-                                highlightColor: const Color(0xFF3F3F3F),
-                                period: const Duration(milliseconds: 500),
-                                child: Container(
-                                  decoration:
-                                      const BoxDecoration(color: Colors.black),
-                                  child: const Text(''),
-                                ),
-                              );
-                      },
-                    ),
+              if (widget.data.cards!.length > 1)
+                Container(
+                  width: 24,
+                  height: 24,
+                  decoration: BoxDecoration(
+                    color: Colors.amber.shade300,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: const Icon(
+                    Icons.add,
+                    color: Colors.black54,
+                    size: 20,
                   ),
                 ),
-              ],
-            ),
+            ],
           ),
+      ],
+    );
+  }
+
+  Container _buildCardContent() {
+    return Container(
+      clipBehavior: Clip.hardEdge,
+      decoration: BoxDecoration(
+        gradient: const LinearGradient(
+          begin: Alignment(-1.0, -1.0),
+          end: Alignment(1.0, 2.0),
+          colors: [Color(0xff5c5ae4), Color(0xffDE4981)],
         ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Container(
-              width: 24,
-              height: 24,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const SizedBox(height: 28),
+          AspectRatio(
+            aspectRatio: 1,
+            child: Container(
+              clipBehavior: Clip.hardEdge,
               decoration: BoxDecoration(
-                color: Colors.amber.shade300,
+                color: const Color(0xff364458),
                 borderRadius: BorderRadius.circular(8),
               ),
-              child: const Icon(
-                Icons.add,
-                color: Colors.black54,
-                size: 20,
+              child: Image.network(
+                widget.data.imageUrl,
+                loadingBuilder: (context, child, loadingProgress) {
+                  bool imageLoaded = false;
+                  if (loadingProgress == null) imageLoaded = true;
+
+                  return showImage && imageLoaded
+                      ? child
+                      : Shimmer.fromColors(
+                          baseColor: const Color(0xff051732),
+                          highlightColor: const Color(0xFF3F3F3F),
+                          period: const Duration(milliseconds: 500),
+                          child: Container(
+                            decoration:
+                                const BoxDecoration(color: Colors.black),
+                            child: const Text(''),
+                          ),
+                        );
+                },
               ),
             ),
-          ],
-        ),
-      ],
+          ),
+        ],
+      ),
     );
   }
 }
